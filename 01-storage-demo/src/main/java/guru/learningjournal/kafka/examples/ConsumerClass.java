d90db278ccfcaf586d259562c6cb77a61cd84fed
Hari <haritheophilus@gmail.com>
2021-09-08 10:38:38 +0530

    testing fom CL

--- 01-storage-demo/src/main/java/guru/learningjournal/kafka/examples/ConsumerClass.java
+++ 01-storage-demo/src/main/java/guru/learningjournal/kafka/examples/ConsumerClass.java
@@ -32,7 +32,7 @@ public class ConsumerClass {
 		KafkaConsumer<Integer, String> consumer = createConsumer(consumerGroup);
 		consumeMessage(consumer, topicName);
 		}else {
-			System.out.println("Enter Topic name and test");
+			System.out.println("Enter Topic name test");
 			System.exit(0);
 		}
 
