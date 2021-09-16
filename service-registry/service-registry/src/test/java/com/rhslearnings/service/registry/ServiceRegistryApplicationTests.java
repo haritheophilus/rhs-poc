edbd42a29e65da794d0f65322b169bdf0e8b1b45
Hari <haritheophilus@gmail.com>
2021-09-16 16:45:01 +0530

    Eureka Service Registry

---
+++ service-registry/service-registry/src/test/java/com/rhslearnings/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhslearnings.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
