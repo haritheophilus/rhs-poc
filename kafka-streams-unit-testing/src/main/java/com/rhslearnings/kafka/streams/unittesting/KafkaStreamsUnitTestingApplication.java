58b6db44b8fc8f1f730729487932d95244027f7e
Hari <haritheophilus@gmail.com>
2021-11-02 17:17:41 +0530

    embedded kafka

---
+++ kafka-streams-unit-testing/src/main/java/com/rhslearnings/kafka/streams/unittesting/KafkaStreamsUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.rhslearnings.kafka.streams.unittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsUnitTestingApplication.class, args);
+	}
+
+}
---
+++ kafka-streams-unit-testing/src/main/java/com/rhslearnings/kafka/streams/unittesting/service/ListenerService.java
@@ -0,0 +1,5 @@
+package com.rhslearnings.kafka.streams.unittesting.service;
+
+public class ListenerService {
+
+}
